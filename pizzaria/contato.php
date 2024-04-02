ab427d43c47186012233514c1158f2ac24606397
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-04-01 22:19:41 -0300

    pizzaria em php

---
+++ pizzaria/contato.php
@@ -0,0 +1,34 @@
+<?php include("cabecalho.php") ?>
+<?php include("menu.php") ?>
+
+<main class="bg-light row pe-3 ps-3">
+    <div class="row">
+        <h2 class="mt-4 h1">Entre em contato</h2>
+        <hr>
+    </div>
+
+    <div class="mt-4  ">
+  <label for="exampleFormControlInput1" class="form-label"></label>
+  <input type="nome" class="form-control" id="exampleFormControlInput1" placeholder="Nome">
+</div>
+
+    <div class=" ">
+  <label for="exampleFormControlInput1" class="form-label"></label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+</div>
+
+<div class=" ">
+  <label for="exampleFormControlInput1" class="form-label"></label>
+  <input type="assunto" class="form-control" id="exampleFormControlInput1" placeholder="Assunto">
+</div>
+
+<div class="">
+  <label for="exampleFormControlTextarea1" class="form-label"></label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Mensagem"></textarea>
+</div>
+
+
+
+
+
+<?php include("rodape.php") ?>
